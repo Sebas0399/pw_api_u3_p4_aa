40b8bea57d3861aa8d61a5b344c7168adaf9678e
Sebas0399 <antony.aguay@gmail.com>
2023-07-20 19:50:15 -0500

    taller31

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.repository.model.Estudiante;
 import com.example.demo.service.IEstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMessage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,14 +17,20 @@ public class EstudianteControllerRestFul {
     private IEstudianteService estudianteService;
 
     @GetMapping(path = "/{cedula}")
-    public Estudiante getEstudianteCedula(@PathVariable String cedula) {
-        return this.estudianteService.getEstudianteCedula(cedula);
+    public ResponseEntity<Estudiante> getEstudianteCedula(@PathVariable String cedula) {
+        Estudiante estudiante=this.estudianteService.getEstudianteCedula(cedula);
+
+        return ResponseEntity.status(227).body(estudiante);
     }
 
     @GetMapping()
-    public List<Estudiante> getEstudiantes() {
+    public ResponseEntity<List<Estudiante>> getEstudiantes() {
+        List<Estudiante> estudiantes=this.estudianteService.getEstudiantes();
+        HttpHeaders cabeceras=new HttpHeaders();
+        cabeceras.add("Detalle mensaje","Ciudadanos consultados exitosamente");
+        cabeceras.add("Valor del API","Incalculable");
 
-        return this.estudianteService.getEstudiantes();
+        return new ResponseEntity<>(estudiantes,cabeceras,228);
     }
     @GetMapping(path = "/buscarProv")
     public List<Estudiante> getEstudiantesProv(@RequestParam String provincia) {
--- src/main/java/com/example/demo/controller/MateriaControllerRestFul.java
+++ src/main/java/com/example/demo/controller/MateriaControllerRestFul.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.repository.model.Materia;
 import com.example.demo.service.IMateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,10 +13,10 @@ import org.springframework.web.bind.annotation.*;
 public class MateriaControllerRestFul {
     @Autowired
     IMateriaService materiaService;
-    @GetMapping(path = "/buscar")
-    public Materia buscarMateria(){
-        String nombre="Historia";
-        return this.materiaService.readByNombreMateria(nombre);
+    @GetMapping(path = "/{nombre}")
+    public ResponseEntity<Materia> buscarMateria(@PathVariable String nombre){
+        Materia materia=this.materiaService.readByNombreMateria(nombre);
+        return ResponseEntity.status(HttpStatus.OK).body(materia);
     }
     @PostMapping(path = "/insertar")
     public void insertarMateria(@RequestBody Materia materia){
